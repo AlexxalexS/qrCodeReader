2ba1678199658d9644758a3fe775993302d52fac
AlexxalexS <37020080+AlexxalexS@users.noreply.github.com>
2021-10-28 22:57:34 +0300

    Initial Commit

---
+++ qrCodeReader/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  qrCodeReader
+//
+//  Created by Alexey on 28.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ qrCodeReader/qrCodeReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  qrCodeReaderApp.swift
+//  qrCodeReader
+//
+//  Created by Alexey on 28.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct qrCodeReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
